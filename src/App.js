e6acc4552b983f94ffacb6f588e52b1d6f705c67
Dooshine <doyungi0304@gmail.com>
2022-06-07 22:10:39 +0900

    [Component]App(){SettingRouteToHomeDetail}
    import{...Route}from'RRD',<Route path='~~:id'>

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {
+BrowserRouter,
+Routes,
+Route,
+} from "react-router-dom"
+import Detail from "./routes/Detail";
+import Home from "./routes/Home"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/hello" element={<h1>Hello</h1>}/>
+        <Route path='/movie/:id' element={<Detail/>}/>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
